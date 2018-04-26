4c563f4947a5ff0290ac9f3313b9c5373ccda5c9
Floyden <keisen95@gmail.com>
2018-04-26 13:18:53 +0200

    implemented quicksort, fixed bugs

--- Aufgabe02/main.cpp
+++ Aufgabe02/main.cpp
@@ -1,6 +1,6 @@
 #include "unit.h"
 
-void bubblesort(int*& feld, int length)
+void bubblesort(unsigned int*& feld, int length)
 {
 	for(int pizza = 0; pizza < length; pizza++)
 	{
@@ -8,13 +8,13 @@ void bubblesort(int*& feld, int length)
 		{
 			if(feld[roastbeef] < feld[roastbeef + 1])
 			{
-                        	tausche(feld,roastbeef,roastbeef+1);
+                tausche(feld,roastbeef,roastbeef+1);
 			}
-		} 
-	}	
+		}
+	}
 }
 
-void insertionsort(int*& feld, int length)
+void insertionsort(unsigned int*& feld, int length)
 {
 	for(int i = 1; i < length; i++)
 	{
@@ -28,7 +28,7 @@ void insertionsort(int*& feld, int length)
 	}
 }
 
-void selectionsort(int*& feld, int length)
+void selectionsort(unsigned int*& feld, int length)
 {
 	for(int i = 0; i < length; i++)
 	{
@@ -42,73 +42,146 @@ void selectionsort(int*& feld, int length)
 	}
 }
 
-void _mergeInner(int*& left, int lLength, int*& right, rLength)
+void _mergeInner(unsigned int*& left, int lLength, unsigned int*& right, int rLength)
 {
 	//split
 	if(lLength > 2)
-		_mergeInner(left, lLength / 2, *(left[lLenght / 2]), (lLength + 1) / 2);
+    {
+        uint32_t* _left = &(left[lLength / 2]);
+        _mergeInner(left, lLength / 2, _left, (lLength + 1) / 2);
+    }
 	if(rLength > 2)
-		_mergeInner(right, rLength / 2, *(right[rLength / 2]), (rLength + 1) / 2);
+    {
+        uint32_t* _right = &(right[rLength / 2]);
+		_mergeInner(right, rLength / 2, _right, (rLength + 1) / 2);
+    }
 	//merge
 	int* field = (int*) malloc((lLength + rLength) * sizeof(int));
 
 	int l = 0;
 	int r = 0;
-	while(i < lLength && j < rLength)
+	while(l < lLength && r < rLength)
 	{
 		if(left[l] < right[r])
 		{
 			field[l + r] = left[l];
-			l++
+			l++;
 		}
 		else
 		{
-			right[l + r] = right[r]
+			right[l + r] = right[r];
 			r++;
 		}
 	}
 
 	if(l != lLength)
-		memcpy(*(field[l + r], *(left[l]), lLength - l));
-	else if(r != lLenght)
-		memcpy(*(field[l + r], *(right[r]), rLength - r));
+		memcpy(&(field[l + r]), &(left[l]), lLength - l);
+	else if(r != rLength)
+		memcpy(&(field[l + r]), &(right[r]), rLength - r);
 
 	memcpy(left, field, lLength + rLength);
 	free(field);
 }
 
-void mergesort(int*& feld, int length)
+void mergesort(unsigned int*& feld, int length)
 {
-	_mergeInner(feld, length / 2, *(feld[length / 2]), (length + 1) / 2);
+    unsigned int* left = &(feld[length / 2]);
+	_mergeInner(feld, length / 2, left, (length + 1) / 2);
 }
 
-void _heapify(int *&feld, int length)
+void _heapify(unsigned int*& feld, int length, int pos)
 {
-	for(int i = 0; i < length; i++)
+	int next = 2 * pos + 1;
+	while(next < length)
 	{
+		if(next + 1 < length && feld[next] < feld[next + 1])
+			next++;
+		if(feld[pos] >= feld[next])
+			break;
+		tausche(feld, pos, next);
+		pos = next;
+		next = 2 * pos + 1;
 	}
 }
 
-void heapsort(int*& feld, int length)
+void heapsort(unsigned int*& feld, int length)
 {
-	
+	for(int i = 0; i < length; i++)
+		_heapify(feld, length - i, i);
+	for(int i = length - 1; i >= 0; i--)
+	{
+		tausche(feld, 0, i);
+		_heapify(feld, i, 0);
+	}
 }
 
-void quicksort1(int*& feld, int length)
+void quicksort(unsigned int*& feld, int length)
 {
+	int pivot = length - 1;
+    int j;
+	for(int i = 0, j = length - 2; i < j; i++)
+    {
+        if(feld[i] <= feld[pivot])
+            continue;
+        else
+        {
+            while(i < j)
+            {
+                if(feld[j] <= feld[pivot])
+                    break;
+                else
+                    j--;
+            }
+            tausche(feld, i, j);
+        }
+    }
+    tausche(feld, j, pivot);
+
+    quicksort(feld, j);
+
+    unsigned int* left = &(feld[j]);
+    quicksort(left, length - j);
 }
 
-void quicksort2(int*& feld, int length)
+void quicksortMedian(unsigned int*& feld, int length)
 {
-}
+    if(feld[0] > feld [length / 2])
+        tausche(feld, 0, length/2);
+    if(feld[length - 1] > feld [length / 2])
+        tausche(feld, length / 2, length - 1);
 
+    int pivot = length - 1;
+    int j;
+	for(int i = 0, j = length - 2; i < j; i++)
+    {
+        if(feld[i] <= feld[pivot])
+            continue;
+        else
+        {
+            while(i < j)
+            {
+                if(feld[j] <= feld[pivot])
+                    break;
+                else
+                    j--;
+            }
+            tausche(feld, i, j);
+        }
+    }
+    tausche(feld, j, pivot);
+
+    quicksortMedian(feld, j);
+
+    unsigned int* left = &(feld[j]);
+    quicksortMedian(left, length - j);
+}
 
 
 int main()
 {
-  const unsigned int length = 32;
+  size_t length = 32;
   unsigned int *feld;
 
   start(1, length, feld);
-  bubblesort(feld, length);
+  //bubblesort(feld, length);
 }
