cd486f0130c5f06d255594e19ef9e0e9dd4c7959
floyden <floyden@fnaa.net>
2018-05-10 21:57:00 +0000

    finished hopefully

--- AufgabeInf01/a3.cpp
+++ AufgabeInf01/a3.cpp
@@ -10,46 +10,86 @@
 //------------------------------------------------------------------------------
 // bitte mit Leben fuellen:
 //------------------------------------------------------------------------------
-
+lint fastpow(lint, lint, lint);
 // Probe
 
 lint PrimTest(lint n, const CSieb& Sieb)
 {
-	if(Sieb[n])
-		return 1;
+	// if(Sieb[n])
+	// 	return 1;
 
 	for(int i = 2; i < n; i++)
 	{
-		if(!Sieb[i])
+		if(i < Sieb.length() && !Sieb[i])
 			continue;
 		if(!(n % i))
 			return i;
 	}
 	// Shouldn't be reached
-	std::cout << "PrimTest: Reached unreachable code\n";
-	return 0;
+	return 1;
 }
 
 // Fermat
 
 lint FermatTest(lint n, lint a)
-{ return 0; }
+{
+	return fastpow(a, n-1, n);
+}
 
 // Miller-Rabin
 
 lint MillerRabinTest(lint n, lint a, lint d, lint r)
-{ return 0; }
+{
+	lint e = 1;
+	for(lint i = 0; i < r; i++) e *= 2;
+	return fastpow(a, e * d, n);
+}
 
 // Hilfsfunktionen
 
 lint fastpow(lint nBase, lint nExp, lint nMod)
-{ return 0; }
+{
+	nBase %= nMod;
+	lint res = 1;
+	while(nExp > 0)
+	{
+		if(nExp & 1)
+			res = (res * nBase) % nMod;
+		nBase = (nBase * nBase) % nMod;
+		nExp >>= 1;
+	}
+	return res;
+}
 
 lint gcd(lint a, lint b)
-{ return 0; }
+{
+	if( a < 1 || b < 1)
+	{
+		std::cout<< "Error < 0 " << std::endl;
+		exit(-1);
+	}
+	lint y = 0;
+	do
+	{
+		y = a%b;
+		a = b;
+		b = y;
+	}while(b!=0);
+	return a;
+
+}
 
 void get_ds(lint n, lint& d, lint& s)
-{ return; }
+{
+	d = n - 1;
+	for (s = 1; s < n; s++)
+	{
+		if(d % 2)
+			return;
+		d /= 2;
+		s++;
+	}
+}
 
 //------------------------------------------------------------------------------
 // Hauptprogramm
@@ -67,17 +107,93 @@ int main()
 	{
 		if(!Sieb[i])
 			continue;
-		for(int j = i + i; i < Sieb.length(); j += i)
+		for(int j = i + i; j < Sieb.length(); j += i)
+		{
 			Sieb[j] = false;
+		}
 	}
 
 	SiebTest(Sieb);
 
 	// PrimTest
-	
+	// for(int i = 1; i <= AnzahlBeispiele; i++)
+	// {
+	// 	lint val = HoleKandidaten(Test::Probe, i);
+	//
+	// 	lint res = PrimTest(val, Sieb);
+	// 	TestErgebnis(Test::Probe, i	, res);
+	// }
+
 	// Fermat-Test
+	for(int i = 1; i <= AnzahlBeispiele; i++)
+	{
+		lint val = HoleKandidaten(Test::Fermat, i);
+
+		bool prim = true;
+		for(int a = 0; a < MaxTestPrimzahl; a++)
+		{
+			if(!Sieb[a] || gcd(a, val) != 1)
+				continue;
+			lint y = FermatTest(val, a);
+			if(y != 1)
+				prim = false;
+
+			TestErgebnis(Test::Fermat, i, y, a);
+		}
+		TestErgebnis(Test::Fermat, i, prim, 0);
+	}
+
 
 	// Miller-Rabin-Test
+	for(int i = 1; i <= AnzahlBeispiele; i++)
+	{
+		lint val = HoleKandidaten(Test::MillerRabin, i);
+
+		bool prim = true;
+		for(int a = 0; prim && a <= MaxTestPrimzahl; a++)
+		{
+			if(!Sieb[a] || gcd(a, val) != 1)
+				continue;
+			lint d, s, tmp;
+			get_ds(val, d, s);
+
+			bool found = false;
+
+			// lint y = MillerRabinTest(val, a, d, 0);
+			// TestErgebnis(Test::MillerRabin, i, y, a, 0);
+			// if(y == 1 || y == val - 1)
+			// 	found = true;
+			// for(lint r = 1; !found && r < s ; r++)
+			// {
+			// 	lint y = MillerRabinTest(val, a, d, r);
+			// 	TestErgebnis(Test::MillerRabin, i, y, a, r);
+			// 	if(y == val - 1)
+			// 		found = true;
+			// 	if(y <= 1)
+			// 		break;
+			// }
+			for(lint r = 0; !found && r < s ; r++)
+			{
+				lint y = MillerRabinTest(val, a, d, r);
+				TestErgebnis(Test::MillerRabin, i, y, a, r);
+
+				if(y == 1)
+				{
+					if(r == 0)
+						found  = true;
+					else
+						break;
+				}
+				else if(y == val - 1 )
+				{
+					found  = true;
+				}
+			}
+			if(!found)
+				prim = false;
+		}
+		TestErgebnis(Test::MillerRabin, i, prim, 0, 0);
+	}
 
 	return 0;
 }
